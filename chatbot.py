78d26c4cedfe5c1943309e0b056d676ee0f43580
Kcandy <83863207+KCandy24@users.noreply.github.com>
2025-09-30 17:02:41 +0800

    feat: train data on english greetings corpus

--- chatbot.py
+++ chatbot.py
@@ -1,14 +1,17 @@
 from chatterbot import ChatBot as cb
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 from conversations import sample_conversations
 
 class EmpathicChatBot():
     def __init__(self):
-        self.name = "John Adams"
+        self.name = "Emio"
         self.chatbot = cb(self.name)
         trainer = ListTrainer(self.chatbot)
         for conversation in sample_conversations:
             trainer.train(conversation)
+            
+        trainer2 = ChatterBotCorpusTrainer(self.chatbot)
+        trainer2.train("greetings")
 
     def get_response(self, message: str):
         if message.lower() == "clear mem":
